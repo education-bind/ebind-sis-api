650562f8f9e1bd5b700af6807465b67762a67da2
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-10-24 15:51:00 +0200

    add circleci pipeline

--- tests/controllers/countries.test.js
+++ tests/controllers/countries.test.js
@@ -52,7 +52,6 @@ describe("Countries", () => {
       )
       .send(invalidCountry)
       .end((err, res) => {
-        console.log(res.body)
         const { data } = res.body;
         expect(res.status).to.equal(badRequest);
         expect(data);
