69e7168f93f084346747b6752628cca4c9bfa356
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-11-06 12:05:08 +0200

    add schools and facilties

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,18 +1,8 @@
 import express from "express";
-import AppError from "../utils/appError";
-import messages from "../utils/customMessages";
-import statusCode from "../utils/statusCodes";
 import apiRouter from "./api/index";
-
-const { endpointNotFound } = messages;
-const { notFound } = statusCode;
 
 const allRoutes = express.Router();
 
 allRoutes.use("/api", apiRouter);
-
-allRoutes.all("*", (req, res, next) => {
-  next(new AppError(endpointNotFound, notFound));
-});
 
 export default allRoutes;
