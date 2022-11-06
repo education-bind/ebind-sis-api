6044b5e141fede1c764ea8e162dcedc09bd9f628
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-11-06 14:56:09 +0200

    add schools and facilties

--- src/app.js
+++ src/app.js
@@ -6,12 +6,10 @@ import helmet from "helmet";
 import globalErrorHandler from "./controllers/error.controller";
 import compression from "compression";
 import allRoutes from "./routers";
-import bodyParser from "body-parser";
 import AppError from "./utils/appError";
 import messages from "./utils/customMessages";
 import statusCode from "./utils/statusCodes";
 
-const { urlencoded, json } = bodyParser;
 const { endpointNotFound } = messages;
 const { notFound } = statusCode;
 
@@ -35,8 +33,8 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(cookieParser());
-app.use(urlencoded());
-app.use(json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(helmet());
 app.use(compression());
 
