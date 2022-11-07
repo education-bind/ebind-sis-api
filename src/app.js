9a8a958038f818b5bd53c763968bbeb87eb51075
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-11-07 16:13:22 +0200

    add schools and facilties

--- src/app.js
+++ src/app.js
@@ -1,85 +1,23 @@
 import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-import helmet from "helmet";
 import globalErrorHandler from "./controllers/error.controller";
-import compression from "compression";
 import allRoutes from "./routers";
 import AppError from "./utils/appError";
 import messages from "./utils/customMessages";
 import statusCode from "./utils/statusCodes";
+import middlewares from "./middlewares";
 
 const { endpointNotFound } = messages;
 const { notFound } = statusCode;
 
 const app = express();
 
-app.use(cors());
-app.enable("trust proxy");
-
-app.use((_, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(helmet());
-app.use(compression());
+middlewares(app);
 
 app.use(allRoutes);
 
 app.use((req, res, next) => {
   next(new AppError(endpointNotFound, notFound));
 });
-
-function print(path, layer) {
-  if (layer.route) {
-    layer.route.stack.forEach(
-      print.bind(null, path.concat(split(layer.route.path)))
-    );
-  } else if (layer.name === "router" && layer.handle.stack) {
-    layer.handle.stack.forEach(
-      print.bind(null, path.concat(split(layer.regexp)))
-    );
-  } else if (layer.method) {
-    console.log(
-      "%s /%s",
-      layer.method.toUpperCase(),
-      path.concat(split(layer.regexp)).filter(Boolean).join("/")
-    );
-  }
-}
-
-function split(thing) {
-  if (typeof thing === "string") {
-    return thing.split("/");
-  } else if (thing.fast_slash) {
-    return "";
-  } else {
-    var match = thing
-      .toString()
-      .replace("\\/?", "")
-      .replace("(?=\\/|$)", "$")
-      .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//);
-    return match
-      ? match[1].replace(/\\(.)/g, "$1").split("/")
-      : "<complex:" + thing.toString() + ">";
-  }
-}
-
-app._router.stack.forEach(print.bind(null, []));
 
 app.use(globalErrorHandler);
 
---
+++ src/middlewares/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import favicon from "serve-favicon";
+import requestMethods from "./requestMethods.js";
+import cors from "./cors";
+import morgan from "morgan";
+import helmet from "helmet";
+
+export default (app) => {
+  if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+  }
+  app.use(requestMethods);
+  app.use(compression());
+  app.use(favicon("public/favicon.ico"));
+  app.use(express.static("public"));
+  app.use(cors);
+  app.use(cookieParser());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(helmet());
+};
